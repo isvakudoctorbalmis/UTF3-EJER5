c73e63f8b03ba41f7181691a025710129c4b2196
ICMDAW1H <48095884+ICMDAW1H@users.noreply.github.com>
2020-11-01 19:30:16 +0100

    Agregar archivos de proyecto.

---
+++ UTF3-EJER5/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace UTF3_EJER5
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resultadoTextBlock.Text += (sender as Button).Tag.ToString();
+
+
+        }
+    }
+}
